ed38ea4cffc9b8df9be5eeba5422322db7978b70
phungvu2012 <phungvuminh2012@gmail.com>
2021-08-02 09:31:58 +0700

    Landrick page

---
+++ dist/assets/js/script.js
@@ -0,0 +1,15 @@
+// var menuItems = document.querySelectorAll("li.nav-item a[href='#']");
+// console.log(menuItems)
+// for (let item = 0; item < menuItems.length; item++) {
+//     const element = menuItems[item];
+//     element.onclick = function(event) {
+//         var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('sub-nav');
+//         if (isParentMenu) {
+//             event.preventDefault();
+//             console.log("true")
+//         }
+//         else {
+//             console.log("false")
+//         }
+//     }
+// }
